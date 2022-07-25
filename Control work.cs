5914394cb3b94f4a3ecd084076fad5292ee8c2b2
barmagor <barmagor@yandex.ru>
2022-07-25 10:42:03 +0300

    1 этап

--- Control work.cs
+++ Control work.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Контрольная работа
+int size;
+Console.WriteLine("Сколько элементов массива вы хотите ввести");
+while (!int.TryParse(Console.ReadLine(), out size) || size <= 0)
+{
+    Console.WriteLine("Ошибка ввода, попробуйте снова");
+    Console.WriteLine("Введите целое число");
+};
+
+string[] array = new string[size];
