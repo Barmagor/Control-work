31a5eba49a476090f97147d2582a24c0dbbf099f
barmagor <barmagor@yandex.ru>
2022-07-25 10:43:29 +0300

    2 этап

--- Control work.cs
+++ Control work.cs
@@ -8,3 +8,17 @@ while (!int.TryParse(Console.ReadLine(), out size) || size <= 0)
 };
 
 string[] array = new string[size];
+FillArray(array);
+
+// функция заполняет массив
+
+void FillArray(string[] arr)
+{
+    for (int x = 0; x < size; x++)
+    {
+        Console.WriteLine("Введите элемент ");
+        while (String.IsNullOrEmpty(arr[x]=Console.ReadLine()))
+            Console.WriteLine("Ошибка ввода, попробуйте снова");
+    }
+
+}
